f9b9d811e3e15384f9f67863e09914e723e4035b
Emanuel Cruz <emanuelcruz091@gmail.com>
2021-04-19 07:52:38 -0300

    Add path for the registration form

--- t17-client/src/routes/routes.js
+++ t17-client/src/routes/routes.js
@@ -1,7 +1,9 @@
 import HomeScreen from "../screens/Home/HomeScreen";
+import FormularioDeRegistro from "../components/FormularioDeRegistro/FormularioDeRegistroComponents";
 
 const routes = [
   { path: "/", Component: HomeScreen },
+  { path: "/register", FormularioDeRegistro}
 ];
 
 export default routes;
