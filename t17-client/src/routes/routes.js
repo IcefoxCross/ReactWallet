8d5ccdb681347452048540092869110b9f4fce3b
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-20 19:01:59 -0300

    Feature: Add basic balance component

--- t17-client/src/routes/routes.js
+++ t17-client/src/routes/routes.js
@@ -1,7 +1,9 @@
+import BalanceScreen from "../screens/Balance/BalanceScreen";
 import HomeScreen from "../screens/Home/HomeScreen";
 
 const routes = [
   { path: "/", Component: HomeScreen },
+  { path: "/balance", Component: BalanceScreen },
 ];
 
 export default routes;
