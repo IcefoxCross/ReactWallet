0d2520d85eb2f914ccd02075c5697f41a0fb9d63
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-20 19:02:54 -0300

    Feature: CReate basic balance component

---
+++ t17-client/src/screens/Balance/BalanceScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BalanceComponent from '../../components/Balance/BalanceComponent'
+
+const BalanceScreen = () => {
+  return (
+    <div>
+      <BalanceComponent />
+    </div>
+  )
+}
+
+export default BalanceScreen
