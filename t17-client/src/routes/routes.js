5c91245b05221b2279426866c69403999befbabb
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-20 19:03:14 -0300

    Fix: Fix not needed routes

--- t17-client/src/routes/routes.js
+++ t17-client/src/routes/routes.js
@@ -1,9 +1,7 @@
-import BalanceScreen from "../screens/Balance/BalanceScreen";
 import HomeScreen from "../screens/Home/HomeScreen";
 
 const routes = [
   { path: "/", Component: HomeScreen },
-  { path: "/balance", Component: BalanceScreen },
 ];
 
 export default routes;
