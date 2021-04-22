ce28184b8e189d25347fd9ba018249a1176423ef
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-22 11:36:27 -0300

    Feature: Add 'Create FTD' routes and constants

--- t17-client/src/constants/constants.js
+++ t17-client/src/constants/constants.js
@@ -8,4 +8,10 @@ export const MESSAGE_EMAIL_EMAIL = "Ingrese un correo valido"
 export const MESSAGE_REQUIRED_EMAIL = "Es necesario que ingrese su correo"
 export const MESSAGE_STRING_PASSWORD = "Ingrese su contraseña"
 export const MESSAGE_MIN_PASSWORD = "Su contraseña tiene que tener un mínimo de 6 caracteres"
-export const MESSAGE_REQUIRED_PASSWORD ="Es necesario que ingrese su contraseña"
+export const MESSAGE_REQUIRED_PASSWORD = "Es necesario que ingrese su contraseña"
+
+//Constants of Create Fixed Term Deposit
+export const MESSAGE_STRING_FTD_AMMOUNT = "Ingrese el monto"
+export const MESSAGE_REQUIRED_FTD_AMMOUNT = "Es necesario que ingrese el monto"
+export const MESSAGE_NOT_A_NUMBER = "Debe ingresar un número"
+export const MESSAGE_NEGATIVE_NUMBER = "El monto debe ser positivo"
--- t17-client/src/routes/routes.js
+++ t17-client/src/routes/routes.js
@@ -1,9 +1,11 @@
 import HomeScreen from "../screens/Home/HomeScreen";
 import FormSignUp from "../components/FormSignUp/FormSignUpComponent";
+import CreateFixedTermDepositScreen from "../screens/CreateFixedTermDeposit/CreateFixedTermDepositScreen";
 
 const routes = [
   { path: "/", Component: HomeScreen },
-  { path: "/register", Component: FormSignUp}
+  { path: "/register", Component: FormSignUp },
+  { path: "/create-ftd", Component: CreateFixedTermDepositScreen },
 ];
 
 export default routes;
