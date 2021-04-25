86740c7b6612306d17a42a02879c211f879ff8a2
Cesar <reuscesar9@gmail.com>
2021-04-25 01:43:34 -0300

    SERVER/Fix: Account field mistype + /Feature: Create Account

--- t17-server/migrations/20210420160605-create-account.js
+++ t17-server/migrations/20210420160605-create-account.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
-      currenctType: {
+      currencyType: {
         type: Sequelize.STRING,
         allowNull: false,
       },
