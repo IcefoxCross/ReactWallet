656021ab52eed70050cea86da33cfd4149ecbb89
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-25 14:43:11 -0300

    Fix: Transaction problems with Account asociation

--- t17-server/migrations/20210422150320-create-transaction.js
+++ t17-server/migrations/20210422150320-create-transaction.js
@@ -4,45 +4,45 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
 
     await queryInterface.createTable("Transactions", {
-        id: {
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER,
-        },
-        amount: {
-            allowNull: false,
-            type: Sequelize.INTEGER,
-        },
-        concept: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-        type: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-        accountId: {
-            allowNull: false,
-            type: Sequelize.INTEGER,
-            onDelete: 'CASCADE',
-            // references: {
-            //   model: 'Accounts',
-            //   key: 'id',
-            // }
-        },
-        createdAt: {
-            allowNull: false,
-            type: Sequelize.DATE,
-        },
-        updatedAt: {
-            allowNull: false,
-            type: Sequelize.DATE,
-        },
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      amount: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      concept: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      accountId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Accounts',
+          key: 'id',
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-      await queryInterface.dropTable("Transactions");
+    await queryInterface.dropTable("Transactions");
   }
 };
--- t17-server/querys/transaction.js
+++ t17-server/querys/transaction.js
@@ -1,9 +1,9 @@
 const db = require("../models");
 const Transaction = db.Transaction;
 
-const createTransaction = async (amount, concept, type, accountId ) => {
+const createTransaction = async (amount, concept, type, accountId) => {
   try {
-    const newTransaction = await Transaction.create({ amount, concept,type,accountId });
+    const newTransaction = await Transaction.create({ amount, concept, type, accountId });
     return newTransaction;
   } catch (err) {
     return err;
