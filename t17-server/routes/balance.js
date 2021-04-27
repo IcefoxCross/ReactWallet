48a55b29ad75c03bf7baa00d61abc38f7ded1f67
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-27 18:09:55 -0300

    Fix: Delete unused files

--- t17-server/routes/balance.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getBalanceByAccount } = require('../controllers/balances');
-
-// Get all fixed terms Deposits by user
-router.get('/:id', getBalanceByAccount);
-
-
-module.exports = router;
