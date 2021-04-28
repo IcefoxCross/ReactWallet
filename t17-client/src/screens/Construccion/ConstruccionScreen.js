3b5d4e0eda86141dfdf6c9183265aab0838be608
Cesar <reuscesar9@gmail.com>
2021-04-28 17:22:14 -0300

    CLIENT/Feature: Construction Screen

--- t17-client/src/screens/Construccion/ConstruccionScreen.js
+++ t17-client/src/screens/Construccion/ConstruccionScreen.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {
+    Typography,
+    Container,
+    Paper,
+    Box
+} from "@material-ui/core";
 
 const ConstructionScreen = () => {
     return (
-        <div></div>
+        <Container maxWidth="sm">
+            <Paper>
+                <Box p={3}>
+                    <Typography variant="h3" display="block">
+                        Pagina en Construccion
+                    </Typography>
+                    <Typography variant="overline" display="block">
+                        Disculpe las molestias
+                    </Typography>
+                </Box>
+            </Paper>
+        </Container>
     );
 };
 
