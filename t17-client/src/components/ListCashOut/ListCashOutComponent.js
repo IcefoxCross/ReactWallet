90a619eefc21e3a10735f01e0584c2e0a430dfd3
Emanuel Cruz <emanuelcruz091@gmail.com>
2021-04-28 09:10:58 -0300

    Add test data

--- t17-client/src/components/ListCashOut/ListCashOutComponent.js
+++ t17-client/src/components/ListCashOut/ListCashOutComponent.js
@@ -14,16 +14,53 @@ import {
 } from "@material-ui/core";
 import useStyles from "./ListCashOutStyles";
 
+const userId = 1
+const accountId = 1;
+
+const dataCashOutList = [
+    {
+        id: 1,
+        amount: "1200",
+        concept:"Egreso numero 1",
+        type: "payment",
+        accountId: accountId,
+        createdAt: (new Date()).toISOString(),
+        updatedAt: (new Date()).toISOString(),
+    },
+    {
+        id: 2,
+        amount: "100",
+        concept: "Egreso numero 2",
+        type: "payment",
+        accountId: accountId,
+        createdAt: (new Date()).toISOString(),
+        updatedAt: (new Date()).toISOString(),
+    },
+    {
+        id: 3,
+        amount: "1000",
+        concept: "Egreso numero 3",
+        type: "payment",
+        accountId: accountId,
+        createdAt: (new Date()).toISOString(),
+        updatedAt: (new Date()).toISOString(),
+    },
+];
+
 export default function ListCashOutComponent() {
     const classes = useStyles();
     const [cashOutList, setCashOutList] = useState([])
     useEffect(() => {
-        //Usar endpoint para traer egresos de dinero
-        setCashOutList(cashOutList);
+        // const fetchAPI = async () => {
+        //     const dataCashOutList = await httpGetOne("cashOut",userId)
+        //     setTopupMoneyList(dataCashOutList.data);
+        // }
+        // fetchAPI();
+        setCashOutList(dataCashOutList);
     }, []);
     return (
         <Container maxWidth="sm">
-            <TableComponent list={cashOutList}></TableComponent>
+            <TableComponent data={cashOutList} title="Egresos"></TableComponent>
         </Container>
     )
 }
--- t17-client/src/components/ListTopupMoney/ListTopupMoneyComponent.js
+++ t17-client/src/components/ListTopupMoney/ListTopupMoneyComponent.js
@@ -14,16 +14,53 @@ import {
 } from "@material-ui/core";
 import useStyles from "./ListTopupMoneyStyles";
 
+const userId = 1;
+const accountId = 1;
+
+const dataTopupMoney = [
+    {
+        id: 1,
+        amount: "1200",
+        concept:"Ingreso numero 1",
+        type: "topup",
+        accountId: accountId,
+        createdAt: (new Date()).toISOString(),
+        updatedAt: (new Date()).toISOString(),
+    },
+    {
+        id: 2,
+        amount: "100",
+        concept: "Ingreso numero 2",
+        type: "topup",
+        accountId: accountId,
+        createdAt: (new Date()).toISOString(),
+        updatedAt: (new Date()).toISOString(),
+    },
+    {
+        id: 3,
+        amount: "1000",
+        concept: "Ingreso numero 3",
+        type: "topup",
+        accountId: accountId,
+        createdAt: (new Date()).toISOString(),
+        updatedAt: (new Date()).toISOString(),
+    },
+];
+
 export default function ListTopupMoneyComponent() {
     const [topupMoneyList, setTopupMoneyList] = useState([])
     useEffect(() => {
-        //Usar endpoint para traer ingresos de dinero
-        setTopupMoneyList(topupMoneyList);
+        // const fetchAPI = async () => {
+        //     const dataTopupMoney = await httpGetOne("topupMoney",userId)
+        //     setTopupMoneyList(dataTopupMoney.data);
+        // }
+        // fetchAPI();
+        setTopupMoneyList(dataTopupMoney);
     }, []);
     const classes = useStyles();
     return (
         <Container maxWidth="sm">
-            <TableComponent list={topupMoneyList}></TableComponent>
+            <TableComponent data={topupMoneyList} title="Ingresos"></TableComponent>
         </Container>
     )
 }
