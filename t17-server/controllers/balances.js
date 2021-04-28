2afab75574b14e2925f6b88b359f1cd93aad02cf
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-28 13:19:04 -0300

    Fix: Change the method to get accountId's.

--- t17-server/controllers/balances.js
+++ t17-server/controllers/balances.js
@@ -2,18 +2,20 @@ const express = require('express');
 const { queryGetAllTransactionsByAccount } = require("../querys/balances");
 const consts = require("../constants/consts");
 const { getBalances } = require('../middlewares/getBalance');
+const { getAllAccountsByUser } = require('../querys/accounts');
 
 
 const getBalanceByAccount = async (req, res) => {
-  const accountId = parseInt((Number(req.params.id) * 2) - 1);
-  const usdAccountId = parseInt(Number(req.params.id) * 2);
-  const arsTopUpTransactionsByAccount = await queryGetAllTransactionsByAccount(accountId, 'topup');
-  const arsPaymentTransactionsByAccount = await queryGetAllTransactionsByAccount(accountId, 'payment');
+  const userId = parseInt(req.params.id);
+  const accountsByUserId = await getAllAccountsByUser(userId);
+  const arsAccountId = accountsByUserId[0].id;
+  const usdAccountId = accountsByUserId[1].id;
+  const arsTopUpTransactionsByAccount = await queryGetAllTransactionsByAccount(arsAccountId, 'topup');
+  const arsPaymentTransactionsByAccount = await queryGetAllTransactionsByAccount(arsAccountId, 'payment');
   const usdTopUpTransactionsByAccount = await queryGetAllTransactionsByAccount(usdAccountId, 'topup');
   const usdPaymentTransactionsByAccount = await queryGetAllTransactionsByAccount(usdAccountId, 'payment');
   const balances = getBalances(arsTopUpTransactionsByAccount, arsPaymentTransactionsByAccount, usdTopUpTransactionsByAccount, usdPaymentTransactionsByAccount)
   if (balances) {
-    console.log(usdAccountId)
     res.status(consts.code_success).send(balances);
   } else {
     res.status(consts.CODE_FAILURE_404);
