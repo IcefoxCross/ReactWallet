88ad6bd935a7f817f62454f11bafec710eb692bf
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-28 14:04:52 -0300

    Fix: Add new consts and validation for controller

--- t17-server/controllers/balances.js
+++ t17-server/controllers/balances.js
@@ -6,19 +6,23 @@ const { getAllAccountsByUser } = require('../querys/accounts');
 
 
 const getBalanceByAccount = async (req, res) => {
-  const userId = parseInt(req.params.id);
-  const accountsByUserId = await getAllAccountsByUser(userId);
-  const arsAccountId = accountsByUserId[0].id;
-  const usdAccountId = accountsByUserId[1].id;
-  const arsTopUpTransactionsByAccount = await queryGetAllTransactionsByAccount(arsAccountId, 'topup');
-  const arsPaymentTransactionsByAccount = await queryGetAllTransactionsByAccount(arsAccountId, 'payment');
-  const usdTopUpTransactionsByAccount = await queryGetAllTransactionsByAccount(usdAccountId, 'topup');
-  const usdPaymentTransactionsByAccount = await queryGetAllTransactionsByAccount(usdAccountId, 'payment');
-  const balances = getBalances(arsTopUpTransactionsByAccount, arsPaymentTransactionsByAccount, usdTopUpTransactionsByAccount, usdPaymentTransactionsByAccount)
-  if (balances) {
-    res.status(consts.code_success).send(balances);
-  } else {
-    res.status(consts.CODE_FAILURE_404);
+  try {
+    const userId = parseInt(req.params.id);
+    const accountsByUserId = await getAllAccountsByUser(userId);
+    const arsAccountId = accountsByUserId.length ? accountsByUserId.filter(acc => acc.currencyType === 'ARS')[0].id : 0;
+    const usdAccountId = accountsByUserId.length ? accountsByUserId?.filter(acc => acc.currencyType === 'USD')[0].id : 0;
+    const arsTopUpTransactionsByAccount = await queryGetAllTransactionsByAccount(arsAccountId, 'topup');
+    const arsPaymentTransactionsByAccount = await queryGetAllTransactionsByAccount(arsAccountId, 'payment');
+    const usdTopUpTransactionsByAccount = await queryGetAllTransactionsByAccount(usdAccountId, 'topup');
+    const usdPaymentTransactionsByAccount = await queryGetAllTransactionsByAccount(usdAccountId, 'payment');
+    const balances = getBalances(arsTopUpTransactionsByAccount, arsPaymentTransactionsByAccount, usdTopUpTransactionsByAccount, usdPaymentTransactionsByAccount)
+    if (balances) {
+      res.status(consts.code_success).send(balances);
+    } else {
+      res.status(consts.CODE_FAILURE_404);
+    }
+  } catch (e) {
+    res.status(consts.CODE_FAILURE_404).send(e);
   }
 };
 
