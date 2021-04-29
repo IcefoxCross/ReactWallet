31639611c2eaea25f60a14822bddbd1ff18556e7
Emanuel Cruz <emanuelcruz091@gmail.com>
2021-04-29 12:08:05 -0300

    Feat: Seeder for transaction

--- t17-server/seeders/20210425183432-demo-transaction.js
+++ t17-server/seeders/20210425183432-demo-transaction.js
@@ -5,11 +5,46 @@ module.exports = {
     await queryInterface.bulkInsert('Transactions', [{
       amount: 150,
       concept: 'transaction',
-      type: 'cashout',
+      type: 'topup',
       accountId: 1,
       createdAt: new Date,
       updatedAt: new Date,
-    }], {});
+    },{
+      amount: 150,
+      concept: 'transaction',
+      type: 'topup',
+      accountId: 1,
+      createdAt: new Date,
+      updatedAt: new Date,
+    },{
+      amount: 5550,
+      concept: 'transaction',
+      type: 'topup',
+      accountId: 2,
+      createdAt: new Date,
+      updatedAt: new Date,
+    },{
+      amount: 3150,
+      concept: 'transaction',
+      type: 'topup',
+      accountId: 1,
+      createdAt: new Date,
+      updatedAt: new Date,
+    },{
+      amount: 1500,
+      concept: 'transaction',
+      type: 'payment',
+      accountId: 2,
+      createdAt: new Date,
+      updatedAt: new Date,
+    },{
+      amount: 500,
+      concept: 'transaction',
+      type: 'payment',
+      accountId: 2,
+      createdAt: new Date,
+      updatedAt: new Date,
+    },], {});
   },
 
   down: async (queryInterface, Sequelize) => {
