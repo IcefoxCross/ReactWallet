43f6f660e8a1422e3e070d48a095c848a178f720
Emanuel Cruz <emanuelcruz091@gmail.com>
2021-04-29 12:59:05 -0300

    Fix: Validate if the user has associated accounts

--- t17-server/controllers/transaction.js
+++ t17-server/controllers/transaction.js
@@ -22,23 +22,24 @@ const createTransaction = (req, res, next) => {
     );
 };
 //Get transaction by type
-const getTransactionsByType = (req, res, next) => {
-  const typeTransaction = req.params.type;
-  const userId = parseInt(req.params.userId);
-  accountsQuery
-      .getAccountsIdByUser(userId)
-      .then((accountsId) => {
-          return transactionQuery.querygetTransactionsByType(
-              typeTransaction,
-              accountsId
-          );
-      })
-      .then((result) => {
-          res.status(consts.code_success).send(result);
-      })
-      .catch((err) =>
-          res.status(consts.code_failure).send({ message: err.message })
-      );
+const getTransactionsByType = async (req, res, next) => {
+    try {
+        const typeTransaction = req.params.type;
+        const userId = parseInt(req.params.userId);
+        const accountsId = await accountsQuery.getAccountsIdByUser(userId);
+        if(accountsId.length !== 0) {
+            const result = await transactionQuery.querygetTransactionsByType(
+                typeTransaction,
+                accountsId
+            );
+            res.status(consts.code_success).send(result);
+        }
+        else{
+          throw new Error("El usuario no tiene cuentas asociadas");
+        }
+    } catch (err) {
+        res.status(consts.code_failure).send({ message: err.message });
+    }
 }
 
 module.exports = { getAllTransactionsByAccount, createTransaction, getTransactionsByType };
